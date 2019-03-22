92d7bcc00a18d8d7b748583d257c1bdb0a01df58
Ярослав С. Иваницкий <YIvanitskiy@avtoprom.by>
2019-03-22 16:16:43 +0300

    advanced-zeros

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ module.exports = function getZerosCount(number, base) {
   Sys = base;
   Sum= 0;
   del = 0;
+  count = 1;
   arr = [2,	3,	5,	7,	11,	13,	17,	19,	23,	29,	31,	37,	41,	43,	47,	53,	59,	61,	67,	71,
     73,	79,	83,	89,	97,	101,	103,	107,	109,	113,	127,	131,	137,	139,	149,	151,	157,	163,	167,	173,
     179,	181, 191,	193,	197,	199,	211,	223,	227,	229,	233,	239,	241,	251];
@@ -11,7 +12,7 @@ module.exports = function getZerosCount(number, base) {
     if (Sys !== 1) {
         if (result == 0) {
           Sys = Math.floor(Sys / arr[i]);
-          if (del < arr[i]) {
+          if (del <= arr[i]) {
           del = arr[i];
           k = del;
           i = -1;
